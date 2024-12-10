79a4f203055f98a4d777c5bb9f6e3794d9c04035
Franciszek Fraszczyk <franciszekfraszczyk@gmail.com>
2024-12-10 23:38:01 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,44 +3,46 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-  //const [gameList, setGameList] = useState([]);
+  //const [searchText, setSearchText] = useState(""); <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+  const [summonername, setInputSN] = useState("");
+  const [summonertag, setInputST] = useState("");
+  const [gameList, setGameList] = useState([]);
 
-  function getPlayerGames() {
+  function getPlayerGames(event){
     axios.get("https://llgo-website-backend.onrender.com/past5Games")
-      .then(function (response) {
-        setGameList(response.data);
-      })
-      .catch(function (error) {
+      .then(function (response){
+        setGameList(response.data)
+      }).catch(function(error){
         console.log(error);
-      });
+      })
   }
 
-  function SummonernameInput() {
-    console.log("Summoner name:", input1);
-    console.log("Summoner Tag:", input2);
-    // You can send these values to an API if needed using axios.post or axios.get
-  }
+ /* function getFullSummoner(){
+    console.log("Name:", setInputSN);    
+    console.log("Tag:", setInputST);
+  }*/
+
+
+  console.log(gameList);
 
   return (
     <div className="App">
-      <h2>Welcome to our proxy app</h2>
-
+      <h2>LLGO MEGA</h2>
+       
       <div>
         <input
           type="text"
-          placeholder="Enter first value"
-          value={input1}
-          onChange={(e) => setInput1(e.target.value)}
+          placeholder="Summoner Name"
+          value={summonername}
+          onChange={(e) => setInputSN(e.target.value)}
         />
         <input
           type="text"
-          placeholder="Enter second value"
-          value={input2}
-          onChange={(e) => setInput2(e.target.value)}
+          placeholder="Summoner Tag"
+          value={summonertag}
+          onChange={(e) => setInputST(e.target.value)}
         />
-        <button onClick={SummonernameInput}>Send Values</button>
+        <button onClick={handleSendValues}>Send Values</button>
       </div>
 
       <button onClick={getPlayerGames}>Get the past 5 games</button>
@@ -48,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
